f4506e5a5b2cde1c8f6dbb6d7ffe80cd5a069d70
Tianrui Li <lit1@ufl.edu>
2020-10-06 00:03:49 -0700

    Adds way to build static files with React and serve those static files on server

---
+++ frontend/src/App/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <body>
+            <h2>Weather.io</h2>
+            <h1>Staying connected in all weather!</h1>
+        </body>
+    )
+}
+
+export default Home;
