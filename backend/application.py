97be9d307b22631ff9cb5513c34cc07ea7d5d8d4
Colin <colin.adams@ufl.edu>
2020-10-22 18:28:49 -0400

    added linux npm script. added backend route for weather api request.

--- backend/application.py
+++ backend/application.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from datagetter import *
+from flask import request
 application = Flask(__name__)
 
 
 @application.route('/')
 def main():
     return 'Hello Weathr!'
+
+@application.route('/info')
+def weathr_info():
+    return(one_call(request.remote_addr, config_data['APIKEY']))
 
 
 if __name__ == "__main__":
