8ac87ab35ed37648da964e777ba0065848d52b70
Tianrui Li <lit1@ufl.edu>
2020-10-22 16:21:07 -0700

    Sets up testing

--- backend/application.py
+++ backend/application.py
@@ -1,9 +1,14 @@
 from flask import Flask
 application = Flask(__name__)
 
+
 @application.route('/')
 def main():
     return 'Hello Weathr!'
+
+
+@application.route('/login', methods=['POST'])
+
 
 if __name__ == "__main__":
     application.run(debug=True)
---
+++ backend/tests/test_e2e_login.py
@@ -0,0 +1,14 @@
+import unittest
+
+from flask_testing import LiveServerTestCase
+
+
+class TestLogin(LiveServerTestCase):
+
+    def create_app(self):
+        # TODO: Read up on __NAME__ and modules
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
