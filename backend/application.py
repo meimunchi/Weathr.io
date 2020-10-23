9aa14ae7287b3e0f7ad42b366623821def250d15
Tianrui Li <lit1@ufl.edu>
2020-10-22 18:16:01 -0700

    Adds a post login endpoint

--- backend/application.py
+++ backend/application.py
@@ -1,5 +1,10 @@
-from flask import Flask
+from flask import Flask, request
+from flask_login import LoginManager
+
 application = Flask(__name__)
+login_manager = LoginManager()
+
+login_manager.init_app(application)
 
 
 @application.route('/')
@@ -8,6 +13,15 @@ def main():
 
 
 @application.route('/login', methods=['POST'])
+def login():
+    req_data = request.get_json()
+    email = req_data['email']
+    password = req_data['password']
+
+    print(email)
+    print(password)
+
+    return req_data
 
 
 if __name__ == "__main__":
