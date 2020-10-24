d7a25a921ff31dda4c338456ee7b19be064cfa2e
John Dillon <41346111+jd3builds@users.noreply.github.com>
2020-10-24 04:29:15 -0400

    database can connect and put a user correctly

--- backend/database.py
+++ backend/database.py
@@ -7,19 +7,19 @@ from botocore.exceptions import ClientError
 # links:
 # aws cli setup: https://aws.amazon.com/cli/
 # dynamodb api: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodb.html
-dynamodb = boto3.client('dynamodb')
+dynamodb = boto3.resource('dynamodb')
 
 
 # ------------- CREATE FUNCTIONS ------------- #
 
 
-def put_user(_uid, _email, _password, _name, _phonenum, _admin, dynamodb=None):
+def put_user(_uid, _email, _password, _name, _phonenum, _admin):
 
     table = dynamodb.Table('weathr-data-dev')
 
     response = table.put_item(
         Item={
-            'uid': _uid,
+            'user_id': _uid,
             'email': _email,
             'password': _password,
             'name': _name,
@@ -38,7 +38,7 @@ def get_user(_uid, dynamodb=None):
     table = dynamodb.Table('weathr-data-dev')
 
     try:
-        response = table.get_item(Key={'uid': _uid})
+        response = table.get_item(Key={'user_id': _uid})
     except ClientError as err:
         print(err.response['Error']['Message'])
     else:
@@ -53,7 +53,7 @@ def update_user(_uid, _email, _password, _name, _phonenum, _admin, dynamodb=None
 
     response = table.update_item(
         Key={
-            'uid': _uid
+            'user_id': _uid
         },
         UpdateExpression="email=:e, password=:p, name=:n, phonenum=:pn, admin=:a",
         ExpressionAttributeValues={
@@ -77,7 +77,7 @@ def delete_user(_uid, dynamodb=None):
     try:
         response = table.delete_item(
             Key={
-                'uid': _uid
+                'user_id': _uid
             }
         )
     except ClientError as err:
@@ -87,4 +87,4 @@ def delete_user(_uid, dynamodb=None):
 
 
 if __name__ == '__main__':
-    print("hi")
+    put_user("1", "xxx@gmail.com", "1234pas", "JohnDoe", "7778", False)
