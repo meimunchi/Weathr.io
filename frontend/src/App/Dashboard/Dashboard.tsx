149390c7f1d3796b9a5aefb1af8a767e2c2f869a
John Dillon <41346111+jd3builds@users.noreply.github.com>
2020-11-08 01:30:32 -0500

    Dashboard is integrated and working

--- frontend/src/App/Dashboard/Dashboard.tsx
+++ frontend/src/App/Dashboard/Dashboard.tsx
@@ -3,41 +3,89 @@ import Axios from "axios";
 
 
 function Dashboard() {
-    // const [weatherData, setWeatherData] = useState(null);
+    const [weatherData, setWeatherData] = useState(null);
 
-    // useEffect(
-    //     () => {
-    //         if () {
-    //
-    //         } else {
-    //
-    //         }
-    //     }, [info]
-    // )
-
-    const loadData = async() => {
-        const stringWeatherData = localStorage.getItem('weather-data')
-        if (stringWeatherData) {
-            return JSON.parse(stringWeatherData)
-        } else {
-                if (navigator.geolocation) {
-                    navigator.geolocation.getCurrentPosition((location) => {
-                        const body = {
-                            latitude: location.coords.latitude,
-                            longitude: location.coords.longitude
-                        }
-                    })
-                }
-
-            const response = await Axios.post('http://localhost:5000/info', );
-            console.log(response.data);
-        }
+    const isPastTime = () => {
+    const strNextUpdateTime = localStorage.getItem('next-update-time');
+    if (!strNextUpdateTime) {
+        return true;
     }
 
-    const weatherData = loadData();
+    const nextUpdateTime = parseInt(strNextUpdateTime);
+    const now = new Date();
+    const currTime = now.getTime();
+
+    return (nextUpdateTime - currTime) < 0;
+}
+
+const nextUpdateTime = () => {
+    const now = new Date();
+    const nextUpdateTime = now.getTime() + 300000;  // In 5 minutes
+    return nextUpdateTime.toString()
+}
+
+useEffect(() => {
+    async function getWeatherData() {
+        // TODO: Fix navigation.geolocation
+        let body = {
+            lat: 29.651634,
+            long: -82.324829
+        }
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(async (location) => {
+                body = {
+                    lat: location.coords.latitude,
+                    long: location.coords.longitude
+                }
+            })
+        }
+        const response = await Axios.post('http://localhost:5000/info', body);
+
+        const resWeatherData = response.data;
+        localStorage.setItem('weather-data', JSON.stringify(resWeatherData));
+        setWeatherData(response.data);
+
+        localStorage.setItem('next-update-time', nextUpdateTime());
+    }
+
+    const storedWeatherData = localStorage.getItem('weather-data')
+    if (storedWeatherData && !isPastTime()) {
+        setWeatherData(JSON.parse(storedWeatherData))
+    } else {
+        getWeatherData();
+    }
+  }, [])
 
     return(
-        <div>Hello</div>
+
+      <div>
+          <h1>Weather Dashboard</h1>
+          <h2>48 Hour Hourly Forecast</h2>
+          {
+              weatherData && <div>
+                  { weatherData.hourly.map(hour =>
+                      <p>Temperature: { hour.temp }F</p>
+                  )}
+              </div>
+          }
+          <h2>7-Day Forecast</h2>
+
+          {
+              weatherData && <div>
+                  { weatherData.daily.map((day, index) =>
+                          <div>
+                          <h3>Day {index+1}</h3>
+                          <p>Max Temp: { day.temp.max}</p>
+                          <p>Min Temp: { day.temp.min}</p>
+                          <p>Chance of Rain: { day.pop * 100}%</p>
+                          <p>Humidity: { day.humidity}%</p>
+                          <p>Cloud Cover: { day.clouds}%</p>
+                          </div>
+                      )}
+              </div>
+          }
+
+      </div>
     )
 }
 
