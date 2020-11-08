bb3276f8e8a7aee93276cc06904a70928384e719
Tianrui Li <lit1@ufl.edu>
2020-11-07 23:50:13 -0800

    Completes SMS functionality

--- backend/controllers/sms.py
+++ backend/controllers/sms.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, request
+import json
 from botocore.exceptions import ClientError
 import boto3
 import os
-from providers.sms_provider import send_message,sns_confirm_subscription, validate_message,formulate_message
+from providers.sms_provider import send_message, sns_confirm_subscription, validate_message, formulate_message
 
-sms_api = Blueprint('sms',__name__)
+sms_api = Blueprint('sms', __name__)
+
 
 # -------------- SMS RELATED PATHS -------------- #
 
@@ -14,21 +16,19 @@ def sms_disaster():
     return 'This is the sms/disaster route!'
 
 
-@sms_api.route('/general',methods=['POST'])
+@sms_api.route('/general', methods=['POST'])
 def sms_general():
-    #take in sns message
     message_type = request.headers['x-amz-sns-message-type']
-    req_body = request.get_json(force=True)
-    print(req_body['Message'])
-    # if message_type == "Notification":
+    if message_type == "Notification":
         # parse message (validate etc)
         # if validate_message(message_type,req_body):
-            
-        #formulate message
-        # message = formulate_message(req_body.messageBody)
-        #obtain destination number
-        # destination_number = '+19044796688'
-        # send_message(destination_number, 'Hello Meng Meng')
-    return req_body
+        req_body = request.get_json(force=True)
+        pinpoint_message = json.loads(req_body['Message'])
 
+        # TODO: Can use phone number as another key to identify
 
+        message = formulate_message(pinpoint_message['messageBody'])
+        send_message(pinpoint_message['originationNumber'], message)
+        return message
+    else:
+        return {'success': False}
