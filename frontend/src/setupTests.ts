84dd3b02e35a48f65d2f667b809ce4c558cfc80e
Tianrui Li <littealeaf.2001@gmail.com>
2020-11-27 11:32:21 -0800

    Adds frontend tests

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -1,5 +1,6 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+// automatically executes before running tests
+
+// react-testing-library renders components to document.body
+// adds jest-dom's custom assertions
+import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
