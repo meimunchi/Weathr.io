3a373e6da8f7a52a1ea8cf5eb6e56df93474fa69
Antoine Ferguson <antoine44f@gmail.com>
2020-11-30 16:32:53 -0500

    added chatbox styling

--- frontend/src/App/Home/home.test.tsx
+++ frontend/src/App/Home/home.test.tsx
@@ -3,8 +3,13 @@ import { render } from '@testing-library/react';
 import Home from './home'
 
 describe('Home component', () => {
+<<<<<<< HEAD
   it ('renders appropriate content', () => {
     const { getByTestId } = render(<Home/>);
+=======
+  it('renders correctly', () => {
+    const { getByTestId } = render(<Home />);
+>>>>>>> df7ccd8... added chatbox styling
 
     expect(getByTestId('intro-header')).toHaveTextContent('Staying connected in all weather');
   })
