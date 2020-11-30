91fd632f47bd25d2f5585e34c83bca258fb2db08
Antoine Ferguson <antoine44f@gmail.com>
2020-11-30 16:32:53 -0500

    added chatbox styling

--- frontend/src/App/ChatBot/chat-bot.tsx
+++ frontend/src/App/ChatBot/chat-bot.tsx
@@ -1,27 +1,33 @@
 import React, { useState } from 'react';
-import bigCloud from '../../assets/big-blue-cloud.png'
-import cloud from '../../assets/small-blue-cloud.png'
+import cloudLogo from '../../assets/cloud-logo.png'
+import smallCloud from '../../assets/small-blue-cloud.png'
 import './chat-bot.css'
+
+function submit() {
+    console.log("yo it's submitted.")
+}
 
 function ChatBotPage() {
     // TODO: Use state for input field
 
     return (
         <div className="chatbot">
-            <div className="top_part">
-                <img src={bigCloud} alt="Weathr Logo" />
-                <p>Chat Bot is here to :) Find out what you can talk about here</p>
-                <img src={cloud} alt="Weathr Logo" />
-
+            <div className="header">
+                <img src={cloudLogo} alt="Weathr Logo" />
+                <p data-testid='cloud-logo'>Chat Bot is here to :) Find out what you can talk about here</p>
+                <img src={smallCloud} alt="Weathr Logo" />
             </div>
+
             <div className="chat-container">
-                <div>
+                <div className="textbox">
                     <p>Hellooo</p>
                     <p>Helooo</p>
                 </div>
+
                 <input></input>
-                <button>Send</button>
+                <button onSubmit={submit}>Send</button>
             </div>
+
         </div>
     )
 }
