df7ccd8cbe6f24156875e0f20ee479d3d32afe20
Antoine Ferguson <antoine44f@gmail.com>
2020-11-30 16:32:53 -0500

    added chatbox styling

--- frontend/src/App/About/about.tsx
+++ frontend/src/App/About/about.tsx
@@ -1,17 +1,17 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import bigCloud from '../../assets/big-blue-cloud.png'
 import './about.css'
 
-function About(){
-    return(
+function About() {
+    return (
         <div className="greeting">
             <h2>About Us : The Golden Eagles</h2>
             <p>Inspired by the lack of quick access to important weather data,
-                we aspired to create a website that would provide statistics such as 
-                wind speed, natural disaster alerts, or just simply temperature from our
-                website or our SMS service.
+            we aspired to create a website that would provide statistics such as
+            wind speed, natural disaster alerts, or just simply temperature from our
+            website or SMS service.
             </p>
-            <img src={bigCloud} alt="Weathr Logo"/>
+            <img src={bigCloud} alt="Weathr Logo" />
         </div>
     )
 }
