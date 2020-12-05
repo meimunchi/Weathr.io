de4d92c68a2e08e14d2a75513ca13bb9f3d26911
Tianrui Li <littealeaf.2001@gmail.com>
2020-12-04 17:30:39 -0800

    Merge

--- frontend/src/App/Home/home.tsx
+++ frontend/src/App/Home/home.tsx
@@ -6,7 +6,7 @@ function Home() {
     return (
         <div className="greeting">
             <h2 data-testid="intro-header">Staying connected <br></br> in all weather!</h2>
-            <p>Text us to receive real-time updates on storms and tips to keep safe</p>
+            <p data-testid="intro">Text us to receive real-time updates on storms and tips to keep safe</p>
             <p>Keep up to date on local weather and tips on one centralized platform!</p>
             <img src={bigCloud} alt="Weathr Logo" />
         </div>
