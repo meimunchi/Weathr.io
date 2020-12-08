b34a3f2c41f4fd8e1ed622df4dfd5d33d7ed5998
Tianrui Li <littealeaf.2001@gmail.com>
2020-12-07 20:34:19 -0800

    Updates frontend files

---
+++ backend/static/precache-manifest.a0a1b1c090ff671fae8d1152bf55cceb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493dd9522b59ee71cc72424d74e15ed2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4d39243a47791e215f9",
+    "url": "/static/css/2.18e4cadb.chunk.css"
+  },
+  {
+    "revision": "030ec10b4f12e47d8418",
+    "url": "/static/css/main.abb1a832.chunk.css"
+  },
+  {
+    "revision": "d4d39243a47791e215f9",
+    "url": "/static/js/2.0cf24c19.chunk.js"
+  },
+  {
+    "revision": "52c99d9a9ef994df4310eecb6633fcd5",
+    "url": "/static/js/2.0cf24c19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "030ec10b4f12e47d8418",
+    "url": "/static/js/main.81832f9a.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "976af67d1444437237e8b536de6eb3d3",
+    "url": "/static/media/Colin.976af67d.png"
+  },
+  {
+    "revision": "0cb03e0be1e4185f0e4ddc877e6affac",
+    "url": "/static/media/John.0cb03e0b.png"
+  },
+  {
+    "revision": "2bf057956fb1f17b07e0978b5a4a27d6",
+    "url": "/static/media/Tianrui.2bf05795.jpg"
+  },
+  {
+    "revision": "02568c27d4ee23f7fff53cc95c0d5e1f",
+    "url": "/static/media/antoine.02568c27.jpg"
+  },
+  {
+    "revision": "c5179b17862293214b2e7998f3540154",
+    "url": "/static/media/victoria.c5179b17.jpg"
+  }
+]);
